32efcc23b769aeda142baf7c897cb3bd99977455
Petrik Diak <diak@petrik.hu>
2023-11-06 17:54:55 +0100

    DONE DEAL

--- main.js
+++ main.js
@@ -6,10 +6,55 @@ const quotes = Array.from(list.quotes);
 function init(){
   document.getElementById('allQuotesButton').addEventListener('click', allQuotes);
   document.getElementById('the').addEventListener('click', theQuotes);
+  document.getElementById('long').addEventListener('click', longQuotes);
+  document.getElementById('dbNumber').addEventListener('click', dbQuotes);
+}
+
+function dbQuotes(){
+  const author = document.getElementById('author').value;
+  const objDBQuotes = quotes.map((quote) => quote.author);
+  let counter = 0;
+  console.log(objDBQuotes);
+  objDBQuotes.forEach((item) => {
+    if(item == author){
+      counter++;
+    }
+  })
+  document.getElementById("inputNumber").value = counter;
+}
+
+function longQuotes(){
+  const objLongList = quotes.map((quote) => quote.quote.length).join(', ');
+  console.log(objLongList);
+  let p = document.createElement('p');
+  let container = document.getElementById("paragraph");
+  p.innerText = objLongList;
+  container.appendChild(p);
 }
 
 function theQuotes(){
-  const objTheQuotes
+  const objTheQuotes = quotes.map((quote) => {
+    return quote.quote;
+  })
+
+  let list = document.getElementById("olList")
+  objTheQuotes.forEach((item) => {
+    if(item.includes(' the ')){
+       item.replace(' the ', ' <b> the </b> '); 
+       return item;
+    }
+    else if(item.includes(' The ')){
+       item.replace(' The ', ' <b> The </b> ');
+       return item;
+    }
+  })
+  objTheQuotes.forEach((item) => {
+    let li = document.createElement("li");
+    li.innerHTML = item;
+    list.appendChild(li);
+  })
+  
+  console.log(objTheQuotes);
 }
 
 function allQuotes(){
