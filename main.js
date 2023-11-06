638fd838efee35ed3ea86e8d5c900e8eed8d0294
Petrik Diak <diak@petrik.hu>
2023-11-06 15:29:55 +0100

    start

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 import './style.css'
+import quotes from './quotes.json'
+
+function init(){
+  const obj = quotes;
+  console.log(obj);
+  document.getElementById('allQuotesButton').addEventListener('click', allQuotes);
+}
+
+function allQuotes(){
+  const objAllQuotes = Array.from(obj).map(quote => {
+    return quote.quote + quote.author;
+  })
+}
+
+
+document.addEventListener('DOMContentLoaded', init);
