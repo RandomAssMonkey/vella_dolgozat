6a91c3cf9ef685755be81a16a7caad2b105dda5e
Petrik Diak <diak@petrik.hu>
2023-11-06 16:46:15 +0100

    1.feladat kész

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
 import './style.css'
-import quotes from './quotes.json'
+import list from './quotes.json'
+
+const quotes = Array.from(list.quotes);
 
 function init(){
-  const obj = quotes;
-  console.log(obj);
   document.getElementById('allQuotesButton').addEventListener('click', allQuotes);
+  document.getElementById('the').addEventListener('click', theQuotes);
+}
+
+function theQuotes(){
+  const objTheQuotes
 }
 
 function allQuotes(){
-  const objAllQuotes = Array.from(obj).map(quote => {
-    return quote.quote + quote.author;
-  })
+  const objAllQuotes = quotes.filter((quote) => {
+     return quote.quote + quote.author;
+  }).sort((a,b) => {
+    if(a.author < b.author){
+      return -1;
+    }
+    else if(a.author > b.author){
+      return 1;
+    }
+    else{
+      return 0;
+    }
+  });
+  console.log(objAllQuotes);
+  let list = document.getElementById("myList");
+        objAllQuotes.forEach((item) => {
+          let q = document.createElement("q");
+          let li = document.createElement("li");
+            li.innerText = item.author;
+            q.innerText = item.quote;
+            li.appendChild(q);
+            list.appendChild(li);
+        });
 }
+
 
 
 document.addEventListener('DOMContentLoaded', init);
